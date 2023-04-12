d6968e19243de5413c85d543974de3a1c4da6d1c
shanu <121938164+gitshanu@users.noreply.github.com>
2023-04-12 10:35:19 +0530

    main

---
+++ Introductory Exercises: Arithmetic.py
@@ -0,0 +1,19 @@
+A=int(input())
+a=int(input())
+b=int(input())
+if A==1:
+    print(a+b)
+elif A==2:
+    print(a-b)
+elif A==3:
+    print(a*b)
+elif A==4:
+    print(a/b)
+elif A==5:
+    print(a%b)
+elif A==6:
+    print(a//b)
+elif A==7:
+    print(a**b)
+else:
+    print('Invalid Input')
