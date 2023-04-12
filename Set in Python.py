d2cb3e7647a8339523fc00c9dc503889c8b44537
shanu <121938164+gitshanu@users.noreply.github.com>
2023-04-12 10:42:24 +0530

    main

---
+++ Set in Python.py
@@ -0,0 +1,8 @@
+n= 'Hey I am walking here captain my captain water water everywhere nor a drop to drink'
+a=n.split()
+print('sentence_list:',a)
+u=set(a)
+print()
+print('sentence_set: ',u)
+print()
+print('Num_unique: ',len(u))
